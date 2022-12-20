98fd7e0fb86eaea33684b9cd7502ec869bd4417b
Aleksandr Shishkov <aleksandr.shishkov@student.um.si>
2022-12-20 10:41:40 +0100

    Fixed local volume

--- docker.sh
+++ docker.sh
@@ -1,2 +1,3 @@
 docker build -t steg:latest .
-docker run --rm -it -v ./files:/source/files steg
+# https://stackoverflow.com/questions/41485217/mount-current-directory-as-a-volume-in-docker-on-windows-10
+docker run --rm -it -v ${PWD}/files:/source/files steg:latest
